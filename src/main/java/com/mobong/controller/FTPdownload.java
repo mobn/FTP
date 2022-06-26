732d241f5be5c1585f11c8c3a9f79ab1fb80ce5f
mobn <10273284@qq.com>
2022-06-27 00:08:05 +0800

    ftpdownload

--- src/main/java/com/mobong/controller/FTPdownload.java
+++ src/main/java/com/mobong/controller/FTPdownload.java
@@ -1,4 +1,19 @@
 package com.mobong.controller;
 
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.IOException;
+
 public class FTPdownload {
+
+    public void FTPdownload(String fileName, String filePath) throws IOException {
+        // TODO - implement FTPdownload.FTPdownload
+        FTPClient ftp = new FTPClient();
+        ftp.connect("localhost");
+        ftp.login("user", "pass");
+        ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+        //ftp.storeFile(fileName, filePath);
+        ftp.disconnect();
+
+    }
 }
